e3b7f41f0a6424cb4f4fc3a676908ef7a06660e5
Exciiiting <45070956+Exciiiting@users.noreply.github.com>
2018-12-04 08:39:40 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,30 +23,30 @@ client.on('message', message => {
       if (!developers.includes(message.author.id)) return;
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.send(`تم تغيير البلاينق الى   ${argresult}`)
-  } else 
+      message.channel.send(`*Your Status* : ** ${argresult}**`) 
+ } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`تَم تغيير الواتشينق الى   ${argresult}`)
+      message.channel.send(`*Your Status* : ** ${argresult}**`)    
   } else 
   if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`تَم تغيير الليسينينق الى   ${argresult}`)
+      message.channel.send(`*Your Status* : **  ${argresult}**`)
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/Randy");
-      message.channel.send(`تم تغييرك حالتك بالتويتش الى   ${argresult}`)
+      message.channel.send(`*Your Status* : **   ${argresult}**`)
   }
   if (message.content.startsWith(adminprefix + 'sn')) {
   client.user.setUsername(argresult).then
-      message.channel.send(`جاري تغيير الأسم لـ ..${argresult} `)
+      message.channel.send(`*Your Status* : ** ..${argresult} **`)
 } else
 if (message.content.startsWith(adminprefix + 'sa')) {
   client.user.setAvatar(argresult);
-    message.channel.send(`جاري تغيير الأفتار... : `);
+    message.channel.send(`*Your Status* : **..${argresult} : `);
 }
 });
 
